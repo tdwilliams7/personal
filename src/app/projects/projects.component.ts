72c7f430c3de575a45037510c811f31f1542e7ac
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-12-21 16:29:11 -0600

    projects now work on tabs displaying a selected app.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  project = {
+    img: 'assets/momentumClone.png',
+    description: `          Static Clone of seriouseats.com. Made with HTML/CSS, Bootstrap, and React. Tried to mimic as much as possible.
+          Links do take you to that site.`
+  }
+    project1 = {
+    img: 'assets/momentumClone.png',
+    description: `          Static Clone of seriouseats.com. Made with HTML/CSS, Bootstrap, and React. Tried to mimic as much as possible.
+          Links do take you to that site.`
+  }
   constructor() { }
 
+  activeProject = {};
+
+  setActive(event){
+    console.log(event.target.attributes.link.textContent);
+    this.activeProject = {
+      img: event.target.src,
+      description: event.target.attributes.description.textContent,
+      link: event.target.attributes.link.textContent;
+    }
+  }
+
   ngOnInit() {
+    this.activeProject = this.project;
   }
 
 }
